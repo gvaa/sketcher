9604f048722db9b5525282ee4b74c1c06022a941
gvaa <golobokin@gmail.com>
2024-04-17 01:10:04 +0200

    Change colors and layout
    Centered the sketchboard and added colors to the initial board and to the page.

--- scripts/sketcher.js
+++ scripts/sketcher.js
@@ -1,24 +1,24 @@
 "use strict";
 
-const pixelContainer = document.querySelector('#container');
+const pixelContainer = document.querySelector('#pixels');
 const newPixel = document.createElement("div");
 newPixel.setAttribute("class", "pixel");
 
 for (let x = 0; x < 16; x++) {
     for (let y = 0; y < 16; y++) {
         pixelContainer.appendChild(newPixel.cloneNode(true));
-        console.log(x,y);
     }
 }
 
 let mouseDown = 0;
-document.body.onmousedown = function() { 
-  ++mouseDown;
+pixelContainer.onmousedown = function() { 
+  mouseDown = 1;
+  console.log(mouseDown);
 }
-document.body.onmouseup = function() {
-  --mouseDown;
+pixelContainer.onmouseup = function() {
+  mouseDown = 0;
+  console.log(mouseDown);
 }
-
 
 let pixels = document.getElementsByClassName("pixel");
 Array.from(pixels).forEach(pixel => {
